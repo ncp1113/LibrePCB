2c51254f028c8d95b2c0a2cd792d75a31dae84ad
U. Bruhin <urbibruhin@bluewin.ch>
2016-10-30 18:37:20 +0100

    Library editor: Add symbol editor widget

---
+++ libs/librepcb/libraryeditor/sym/symboleditorwidget.cpp
@@ -0,0 +1,121 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <QtWidgets>
+#include "symboleditorwidget.h"
+#include "ui_symboleditorwidget.h"
+#include <librepcb/common/gridproperties.h>
+#include <librepcb/common/graphics/graphicsscene.h>
+#include <librepcb/library/sym/symbol.h>
+#include <librepcb/workspace/workspace.h>
+#include <librepcb/workspace/settings/workspacesettings.h>
+
+/*****************************************************************************************
+ *  Namespace
+ ****************************************************************************************/
+namespace librepcb {
+namespace library {
+namespace editor {
+
+/*****************************************************************************************
+ *  Constructors / Destructor
+ ****************************************************************************************/
+
+SymbolEditorWidget::SymbolEditorWidget(workspace::Workspace& ws, LibraryEditor& editor,
+        const FilePath& fp, QWidget* parent) throw (Exception) :
+    EditorWidgetBase(ws, editor, fp, parent), mUi(new Ui::SymbolEditorWidget),
+    mGridProperties(new GridProperties()), mGraphicsScene(new GraphicsScene())
+{
+    mUi->setupUi(this);
+    mUi->graphicsView->setUseOpenGl(mWorkspace.getSettings().getAppearance().getUseOpenGl());
+    mUi->graphicsView->setGridProperties(*mGridProperties);
+    mUi->graphicsView->setScene(mGraphicsScene.data());
+    setWindowIcon(QIcon(":/img/library/symbol.png"));
+
+    // insert category list editor widget
+    mCategoriesEditorWidget.reset(new ComponentCategoryListEditorWidget(mWorkspace, this));
+    int row;
+    QFormLayout::ItemRole role;
+    mUi->formLayout->getWidgetPosition(mUi->lblCategories, &row, &role);
+    mUi->formLayout->setWidget(row, QFormLayout::FieldRole, mCategoriesEditorWidget.data());
+
+    mSymbol.reset(new Symbol(fp, false)); // can throw
+    setWindowTitle(mSymbol->getName(mLocaleOrder));
+    mUi->lblUuid->setText(QString("<a href=\"%1\">%2</a>").arg(
+        mSymbol->getFilePath().toQUrl().toString(), mSymbol->getUuid().toStr()));
+    mUi->lblUuid->setToolTip(mSymbol->getFilePath().toNative());
+    mUi->edtName->setText(mSymbol->getName(mLocaleOrder));
+    mUi->edtDescription->setText(mSymbol->getDescription(mLocaleOrder));
+    mUi->edtKeywords->setText(mSymbol->getKeywords(mLocaleOrder));
+    mUi->edtAuthor->setText(mSymbol->getAuthor());
+    mUi->edtVersion->setText(mSymbol->getVersion().toStr());
+    mCategoriesEditorWidget->setUuids(mSymbol->getCategories());
+    mUi->cbxDeprecated->setChecked(mSymbol->isDeprecated());
+}
+
+SymbolEditorWidget::~SymbolEditorWidget() noexcept
+{
+}
+
+/*****************************************************************************************
+ *  Public Slots
+ ****************************************************************************************/
+
+bool SymbolEditorWidget::save() noexcept
+{
+    try {
+        QString name = mUi->edtName->text().trimmed();
+        if (name.isEmpty()) {
+            throw RuntimeError(__FILE__, __LINE__, QString(), tr("The name must not be empty."));
+        }
+        Version version(mUi->edtVersion->text().trimmed());
+        if (!version.isValid()) {
+            throw RuntimeError(__FILE__, __LINE__, QString(), tr("The version number is invalid."));
+        }
+
+        mSymbol->setName("en_US", name);
+        mSymbol->setDescription("en_US", mUi->edtDescription->text().trimmed());
+        mSymbol->setKeywords("en_US", mUi->edtKeywords->text().trimmed());
+        mSymbol->setAuthor(mUi->edtAuthor->text().trimmed());
+        mSymbol->setVersion(version);
+        mSymbol->setCategories(mCategoriesEditorWidget->getUuids());
+        mSymbol->setDeprecated(mUi->cbxDeprecated->isChecked());
+        mSymbol->save();
+        return EditorWidgetBase::save();
+    } catch (const Exception& e) {
+        QMessageBox::critical(this, tr("Save failed"), e.getUserMsg());
+        return false;
+    }
+}
+
+/*****************************************************************************************
+ *  Private Methods
+ ****************************************************************************************/
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace editor
+} // namespace library
+} // namespace librepcb
